26c90c5abf2ee2d4081cffd5c99baf283c131e32
Mark Kim <realtg2k@gmail.com>
2019-05-16 15:21:51 -0700

    QuickSort works

--- sorting/QuickSort.cpp
+++ sorting/QuickSort.cpp
@@ -4,12 +4,42 @@
 #include <random>
 
 
-/* QuickSort */ 
-void QuickSort (std::vector<int>& input) {
+int Partition (std::vector<int>& input, int lo, int hi) {
+    srand(time(NULL)); 
+    int random = lo + rand() % (hi - lo);   // generate random index between lo and hi
+
+    std::swap(input[random], input[hi]);    // move random index to end
+
+    int pivot = hi;     // now pivot is in the end
+
+    int left = lo - 1;  // this will be the most right index in smaller part
+  
+    for (int i = lo; i <= hi - 1; ++i) { 
+        if (input[i] <= input[pivot]) { // if curr i element smaller than or equal to pivot
+            ++left;
+            std::swap(input[left], input[i]);
+        } 
+    } 
+    std::swap(input[left + 1], input[hi]); 
+
+    return (left + 1); 
 
 }
 
+/* QuickSort */ 
+void QuickSort (std::vector<int>& input, int lo, int hi) {
+    if (lo < hi ) {
+        /* pick random pivot index */
+        int pivot = Partition(input, lo, hi);
 
+        QuickSort(input, lo, pivot - 1);
+        QuickSort(input, pivot + 1, hi);
+    }
+}
+
+void QuickSort (std::vector<int>& input) {
+    QuickSort(input, 0, input.size() - 1);
+}
 
 
 
@@ -27,9 +57,15 @@ int main (int argc, const char * argv[]) {
         input[i] = dist(rng);
     }
     
-
-    //MergeSortTD(input, 0, input.size() - 1);
     QuickSort(input);
+
+    // std::vector<int> test = {10,7,8,9,1,5};
+    // QuickSort(test);
+    // for (int&x : test) {
+    //     std::cout << x << std::endl;
+    // }
+
+
 
     assert(std::is_sorted(input.begin(), input.end()));
 
