2ae6e1f5618586c257a19dbda64396b85e12ed7b
Mark Kim <realtg2k@gmail.com>
2019-05-16 17:35:01 -0700

    Insertion Sort added

---
+++ sorting/InsertionSort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+
+void InsertionSort (std::vector<int>& input) {
+
+    for (int i = 1; i < input.size(); ++i) {
+        int key = input[i];
+        int j = i - 1;
+
+        while (j >= 0 && input[j] > key) {
+            input[j + 1] = input[j];
+            --j;
+        }
+        input[j + 1] = key;
+    }
+}
+
+
+int main (int argc, const char * argv[]) {
+    
+    srand(time(NULL));
+    u_int input_sz = rand() % 10000;
+    std::vector<int> input(input_sz);    
+
+    /* generating a vector with random number range(from,to) */
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(0,input_sz);
+    for (size_t i = 0; i < input_sz; ++i) {
+        input[i] = dist(rng);
+    }
+    
+    InsertionSort(input);
+
+    // std::vector<int> test = {10,7,8,9,1,5};
+    // InsertionSort(test);
+    // for (int&x : test) {
+    //     std::cout << x << std::endl;
+    // }
+
+
+
+    assert(std::is_sorted(input.begin(), input.end()));
+
+    return 0;
+}
+
+
